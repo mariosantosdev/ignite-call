931a2389c4ad601b0a0c6c75c22ffcf23c841bb4
Mário Santos <mariodev7@gmail.com>
2023-04-17 23:57:07 -0300

    feat(schedule-page): created base page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,62 @@
+import Head from 'next/head'
+import { Container, UserHeader } from './styles'
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '~/lib/prisma'
+
+interface SchedulePageProps {
+  user: {
+    username: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function SchedulePage({ user }: SchedulePageProps) {
+  return (
+    <>
+      <Head>
+        <title>Ignite Call - Agenda {user.username}</title>
+        <meta name="robots" content="noindex" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.username}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+      </Container>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params!.username)
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (!user) return { notFound: true }
+
+  return {
+    props: {
+      user: {
+        username: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
