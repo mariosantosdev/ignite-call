5e402b15762ef57029674840172223f9bf17c57a
Mário Santos <mariodev7@gmail.com>
2023-04-20 12:02:33 -0300

    feat(calendar-step): created simple component

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,41 @@
+import { Calendar } from '~/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+  const isDateSelected = true
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>terça-feira, 20 de setembro</TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+            <TimePickerItem>13:00</TimePickerItem>
+            <TimePickerItem>14:00</TimePickerItem>
+            <TimePickerItem>15:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
