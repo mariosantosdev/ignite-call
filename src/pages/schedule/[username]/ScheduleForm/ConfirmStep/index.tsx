49bdde1f47c4e76109b18f7157775241c32a8176
Mário Santos <mariodev7@gmail.com>
2023-04-20 12:02:53 -0300

    feat(confirm-step): create simple component

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,73 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { z } from 'zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const confirmFormSchema = z.object({
+  name: z.string().min(3, 'O nome precisa no mínimo 3 caracteres'),
+  email: z.string().email('Digite um e-mail válido'),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormSchema = z.infer<typeof confirmFormSchema>
+
+export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormSchema>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+
+  const handleConfirmScheduling: SubmitHandler<ConfirmFormSchema> = () => {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome Completo</Text>
+        <TextInput placeholder="Seu Nome" {...register('name')} />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="johndoe@example.com"
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
+      </label>
+
+      <label>
+        <Text size="sm">Observaçoes</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
