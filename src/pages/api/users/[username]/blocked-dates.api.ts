bd5b9075ecd3a91f556b69b91c1f52018ff4f6ea
Mário Santos <mariodev7@gmail.com>
2023-04-20 13:18:30 -0300

    feat(blocked-dates-api): create api route to get blocked dates

---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '~/lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { year, month } = req.query
+
+  if (!year) {
+    return res.status(400).json({ message: 'Year not provided' })
+  }
+
+  if (!month) {
+    return res.status(400).json({ message: 'Month not provided' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: { week_day: true },
+    where: { user_id: user.id },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter(
+    (weekDay) =>
+      !availableWeekDays.some(
+        (availableWeekDay) => availableWeekDay.week_day === weekDay,
+      ),
+  )
+
+  res.json({ blockedWeekDays })
+}
