98973c1634a9e7aea41ec2cd8fa2886cb983a218
Mário Santos <mariodev7@gmail.com>
2023-04-20 12:54:02 -0300

    feat(availability-api): create api route to get availability times

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,74 @@
+import dayjs from 'dayjs'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '~/lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.status(200).json({ possibleTimes: [], availabilityTimes: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.status(200).json({ possibleTimes: [], availabilityTimes: [] })
+  }
+
+  const {
+    time_end_in_minutes: timeEndInMinutes,
+    time_start_in_minutes: timeStartInMinutes,
+  } = userAvailability
+  const startHour = timeStartInMinutes / 60
+  const endHour = timeEndInMinutes / 60
+
+  const possibleTimes = Array.from({
+    length: endHour - startHour,
+  }).map((_, index) => startHour + index)
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: { date: true },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availabilityTimes = possibleTimes.filter(
+    (time) =>
+      !blockedTimes.some((blockedTime) => blockedTime.date.getHours() === time),
+  )
+
+  res.status(200).json({ possibleTimes, availabilityTimes })
+}
